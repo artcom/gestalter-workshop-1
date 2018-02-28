8aeacd511ef997654031de3798487cb76f498609
Khaled <khaled.reguieg@artcom.de>
2018-02-28 10:38:17 +0100

    Initial commit.

---
+++ Assets/Scripts/MoveText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveText : MonoBehaviour {
+
+	public GameObject starwarsCanvas;
+	public GameObject flatText;
+	public Animator cinematicAnimator;
+
+	bool moveText;
+
+	// Use this for initialization
+	void Start () {
+			
+		Invoke("HideText",3);
+		
+	}
+	
+	void HideText() {
+		
+		starwarsCanvas.SetActive(!starwarsCanvas.activeSelf);
+		moveText = true;
+		Invoke("StartAnimation", 18);
+		//cinematicAnimator.Play("CameraAnimation");
+	}
+	void StartAnimation() {
+
+		cinematicAnimator.Play("CameraAnimation");
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if(moveText) {
+
+			flatText.transform.Translate(Vector3.back * (Time.deltaTime*0.5f));
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gestalter-workshop-1
 The workshop #1 to teach our designers Unity.
+## Setup
+Just clone this repository in your working directory and open it with Unity (preferable 5.6.3p4).
+## Scene
+The main scene is a sun system where our desigenr can explore the concepts of Unity by building our solarsystem and polish the scene.
